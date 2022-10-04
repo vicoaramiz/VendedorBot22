932a050458367c14176df391231113af97532d88
aramiz <Aramiz@gmail.com>
2022-10-04 11:44:15 -0400

    agregamos quick reply

--- Facebook/facebookBot.js
+++ Facebook/facebookBot.js
@@ -144,7 +144,7 @@ async function handleQuickReply(senderId, quickReply, messageId) {
   sendToDialogFlow(senderId, quickReplyPayload);
 }
 
-async function handleDialogFlowAction(
+function handleDialogFlowAction(
   sender,
   action,
   messages,
@@ -152,6 +152,23 @@ async function handleDialogFlowAction(
   parameters
 ) {
   switch (action) {
+    case "Codigo.quickReply.action":
+      sendQuickReply(sender, "ejemplo de quick reply",
+        [
+          {
+            content_type: "text",
+            title: "Si",
+            payload: "si_acepto",
+            image_url: "https://www.hackingchinese.com/wp-content/uploads/2015/09/check-37583_1280.png"
+          },
+          {
+            payload: "no_acepto",
+            title: "No",
+            content_type: "text",
+            image_url: "https://freepngimg.com/thumb/cancel_button/25982-7-cancel-button-transparent.png"
+          }
+        ]);
+      break;
     default:
       //unhandled action, just send back the text
       handleMessages(messages, sender);
